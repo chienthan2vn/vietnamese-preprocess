75899ceaf94395054c61b35719043c2e04b8468e
Kanna <thuanluong19102001@266432.onmicrosoft.com>
2024-09-22 16:26:59 +0700

    Update version 0.0.1

--- setup.py
+++ setup.py
@@ -21,6 +21,6 @@ setuptools.setup(
         "Operating System :: OS Independent",
     ],
     package_dir = {"": "src"},
-    packages = setuptools.find_packages(where="vietnamese-preprocess"),
+    packages = setuptools.find_packages(where="src"),
     python_requires = ">=3.6"
 )
